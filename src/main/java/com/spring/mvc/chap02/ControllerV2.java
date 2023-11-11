8ba598ff5ba4dc12abec3514d8473a968b8c54ad
conan <sarimiro@naver.com>
2023-11-11 14:17:05 +0900

    #4 . ModelAndView 객체로 JSP로 데이터 보내기

--- src/main/java/com/spring/mvc/chap02/ControllerV2.java
+++ src/main/java/com/spring/mvc/chap02/ControllerV2.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+
+import static java.util.List.of;
 
 @Controller
 @RequestMapping("/model")
@@ -22,16 +25,30 @@ public class ControllerV2 {
         System.out.println("취미 안녕~");
 
         String name = "짹짹이";
-        List<String> hobbyList = List.of("전깃줄 앉아있기", "좁쌀주워먹기", "하하호호", "룰루랄라", "멍때리기");
+        List<String> hobbyList = of("전깃줄 앉아있기", "좁쌀주워먹기", "하하호호", "룰루랄라", "멍때리기");
         model.addAttribute("userName", name);
         model.addAttribute("hobbies", hobbyList);
 
         return "chap02/hobbies";
     }
+
+    // == 2. ModelAndView 객체 사용
     @GetMapping("/hobbies2")
-    public String hobbies2() {
+    public ModelAndView hobbies2() {
         System.out.println("취미2 안녕~");
-        return "";
+        // jsp로 보낼 데이터
+        String name = "냥냥이";
+        List<String> hList = List.of("사마귀랑 놀기", "낮잠 자기");
+
+        // jsp로 보낼 데이터를 ModelAndView에 담기
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("userName", name);
+        mv.addObject("hobbies", hList);
+
+        // view의 데이터를 따로 담아줌
+        mv.setViewName("chap02/hobbies");
+
+        return mv;
     }
 
 }
