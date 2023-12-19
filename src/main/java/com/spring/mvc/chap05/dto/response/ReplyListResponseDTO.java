b37dfec2f4551d9b7a96cacd0063010776cfbb7c
conan <sarimiro@naver.com>
2023-12-19 10:51:06 +0900

    #24. REST API 댓글 목록 조회하기

---
+++ src/main/java/com/spring/mvc/chap05/dto/response/ReplyListResponseDTO.java
@@ -0,0 +1,15 @@
+package com.spring.mvc.chap05.dto.response;
+import com.spring.mvc.chap05.dto.response.ReplyDetailResponseDTO;
+import lombok.*;
+
+import java.util.List;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReplyListResponseDTO {
+    private int count;  // 총 댓글 수
+    private List<ReplyDetailResponseDTO> replies;  // 실제 댓글 리스트
+}
